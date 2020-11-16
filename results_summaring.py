7ae041ba5986cc6782ce2c9eddcece172514be5b
Andrzej97 <szafand@gmail.com>
2020-11-16 11:51:42 +0100

    First part of tester refactoring

--- results_summaring.py
+++ results_summaring.py
@@ -1,4 +1,10 @@
 import json
+import csv
+
+FILE_IN = 'tmp.json'
+PRINT_SUMARY = True
+SAVE_TO_FILE = True
+FILE_OUT = 'results.csv'
 
 
 def main():
@@ -6,17 +12,25 @@ def main():
     tp = 0
     fp = 0
     fn = 0
-    with open('tmp.json') as json_file:
+    with open(FILE_IN) as json_file:
         data = json.load(json_file)
         for elem in data:
             annotations += elem['annotation_count']
             tp += elem['true_positive']
             fp += elem['false_positive']
             fn += elem['false_negative']
-    print('Total peaks:\t\t' + str(annotations))
-    print('True positives:\t\t' + str(tp))
-    print('False positives:\t' + str(fp))
-    print('False negatives:\t' + str(fn))
+    if SAVE_TO_FILE:
+        with open(FILE_OUT, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            writer.writerow(['Total peaks', annotations])
+            writer.writerow(['TP', tp])
+            writer.writerow(['FP', fp])
+            writer.writerow(['FN', fn])
+    if PRINT_SUMARY:
+        print('Total peaks:\t\t' + str(annotations))
+        print('True positives:\t\t' + str(tp))
+        print('False positives:\t' + str(fp))
+        print('False negatives:\t' + str(fn))
 
 
 if __name__ == '__main__':
